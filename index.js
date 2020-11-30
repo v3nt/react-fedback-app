11f6525bc0abc773f1c1b179bf4fffb0874d026d
v3nt <dan@jynk.net>
2020-11-30 11:42:57 +0000

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // common js module used for this project
 const express = require("express");
-
-// ES2015 modules.
+// ES2015 modules :
 // import express from 'express' //
 
 const passport = require("passport");
@@ -12,10 +11,22 @@ const app = express();
 // sometimes you use several express apps in a project.
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-  })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// route handler, ie localhost:5000/auth/google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 const PORT = process.env.PORT || 5000;
