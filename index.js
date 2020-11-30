96fdf2ae46ffbbfe64f3236b263d94d7b85a4b54
v3nt <dan@jynk.net>
2020-11-30 16:03:01 +0000

    mongoose keys setup

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ require("./services/passport.js"); // required but ref never used as nothing ret
 // ES2015 modules :
 // import express from 'express' //
 //
-const authRoutes = require("./routes/authRoutes");
+// const authRoutes = require("./routes/authRoutes");
+mongoose = require("mongoose");
+const keys = require("./config/keys");
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 // authRoutes(app);
 // or
 require("./routes/authRoutes")(app); // app ius passed into arrow function in aR.js
-
-// sometimes you use several express apps in a project.
 
 const PORT = process.env.PORT || 5000;
 
