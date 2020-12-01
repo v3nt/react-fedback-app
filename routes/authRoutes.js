77f2aab45ac5296336e9378dbebd887c3f5b4234
v3nt <dan@jynk.net>
2020-12-01 10:06:53 +0000

    logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 
 // route handler, ie localhost:5000/auth/google
-
+// where we reate all the urls.
 module.exports = (app) => {
   // app object is defined in index.js, pre ES5
   app.get(
@@ -13,6 +13,11 @@ module.exports = (app) => {
 
   //for authentication. So when someoen hist `/api/current_user` it will run this function.
   app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get("/api/logout", (req, res) => {
+    req.logout(); // makes passport kill the session / coockie.
     res.send(req.user);
   });
 };
