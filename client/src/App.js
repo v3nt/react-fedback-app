aac2087e66e1faa8012c747fa43cd603728d2a2e
v3nt <dan@jynk.net>
2020-12-02 10:21:03 +0000

    localhost proxy

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>!</p>
+        <a href="/auth/google">Signin with google !!!!</a> !
         <a
           className="App-link"
           href="https://reactjs.org"
