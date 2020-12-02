7fbaacc10fc5c2f9d32aba123986c5556621edcb
v3nt <dan@jynk.net>
2020-12-02 08:39:22 +0000

    prod keys removed

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// dev.js, don't commit.
+const { use } = require("passport");
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
