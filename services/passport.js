d3703bd2c9ee6c6631d284d9c86a15e8448686c8
v3nt <dan@jynk.net>
2020-12-03 11:55:39 +0000

    front end prep router

--- services/passport.js
+++ services/passport.js
@@ -24,21 +24,26 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken, refreshToken, profile, done) => {
-      // when user successfully logs in we run this callback
-      // creates `async` action so needs a promise
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser); // null = no error here, user record.
-        } else {
-          //  // This creates a record of a new user model instance. A single record inside the collection. lso async so needs another promise.
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
 
-      //
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+
+      //   old version. User.findOne({ googleId: profile.id }).then((existingUser) => {
+      //   if (existingUser) {
+      //     done(null, existingUser); // null = no error here, user record.
+      //   } else {
+      //     //  // This creates a record of a new user model instance. A single record inside the collection. lso async so needs another promise.
+      //      new User({ googleId: profile.id })
+      //       .save()
+      //       .then((user) => done(null, user));
+      //   }
+      // });
+
       // console.log("accessToken", accessToken);
       // console.log("refreshToken", refreshToken);
       // console.log("profile", profile);
