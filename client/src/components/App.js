f76f1ccd894d71605575d2a81b1629c0bbc93df9
v3nt <dan@jynk.net>
2020-12-03 12:51:38 +0000

    mock com crated

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+/// Comps
 import Header from "./Header";
+import Footer from "./Footer";
+
+// Dummy comps
+const Dashboard = () => <h2>Dash</h2>;
+const SurveyNew = () => <h2>Survey new</h2>;
+const Landing = () => <p>Landin</p>;
 
 const App = () => {
   return (
     <div>
       <Header />
       <p>Hi there</p>
+      <Landing />
+      <Dashboard />
+      <SurveyNew />
+      <Footer />
     </div>
   );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h2>Header</h2>
+    </div>
+  );
 };
 
 export default Header;
