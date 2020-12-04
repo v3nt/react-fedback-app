e47f74142db795c7ff312390b210941c562fb49b
v3nt <dan@jynk.net>
2020-12-04 13:36:05 +0000

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,21 +2,33 @@ import React from "react";
 import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return "still loading...";
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Google Login</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
-    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="/" className="left brand-logo">
             Emaily
           </a>
-          <ul id="nav-mobile" className="right ">
-            <li>
-              <a href="sass.html">Google Login</a>
-            </li>
-            <li>
-              <a href="badges.html">Logout</a>
-            </li>
+          <ul id="nav-mobile" className="right">
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
