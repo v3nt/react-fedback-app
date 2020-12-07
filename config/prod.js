18e3d482bffb489ad8ff7b280ba9dfa7c7d9b49d
v3nt <dan@jynk.net>
2020-12-07 15:38:48 +0000

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
-// dev.js, don't commit.
+// https://console.developers.google.com/apis/credentials/oauthclient/162192078766-7p8akmg8f4b05pjdpatdqg5uo8t3itoh.apps.googleusercontent.com?project=emaily-prod-297319
 const { use } = require("passport");
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
