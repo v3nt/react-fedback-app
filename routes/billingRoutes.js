52c244d3c1e47d6ab0dc5881b5096f7aa76e6ff0
v3nt <dan@jynk.net>
2020-12-07 19:19:49 +0000

    charge working

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -5,7 +5,15 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 //express does not parse the result `npm i body-parser`
 
 module.exports = (app) => {
-  app.post("/api/stripe", (req, res) => {
-    console.log(req.body);
+  console.log("/api/stripe");
+  app.post("/api/stripe", async (req, res) => {
+    // console.log(req.body);
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 credits",
+      source: req.body.id,
+    });
+    console.log(charge);
   });
 };
