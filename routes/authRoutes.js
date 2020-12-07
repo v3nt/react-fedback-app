2dd53ec79df99191975175e064fe2ec6488d9478
v3nt <dan@jynk.net>
2020-12-07 12:38:21 +0000

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,22 @@ const passport = require("passport");
 
 // route handler, ie localhost:5000/auth/google
 // where we reate all the urls.
+
 module.exports = (app) => {
   // app object is defined in index.js, pre ES5
   app.get(
     "/auth/google",
     passport.authenticate("google", { scope: ["profile", "email"] })
   );
-  // 2nd route handler
-  app.get("/auth/google/callback", passport.authenticate("google"));
+
+  // 2nd route handler. Passport is actually a middleware.
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
   //for authentication. So when someoen hist `/api/current_user` it will run this function.
   app.get("/api/current_user", (req, res) => {
@@ -17,7 +25,7 @@ module.exports = (app) => {
   });
 
   app.get("/api/logout", (req, res) => {
-    req.logout(); // makes passport kill the session / coockie.
-    res.send(req.user);
+    req.logout(); // makes Passport middleware kill the session / cookie.
+    res.redirect("/");
   });
 };
