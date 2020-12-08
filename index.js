cdcf6323155c9311aae5aa4aaed155cc96b119f8
v3nt <dan@jynk.net>
2020-12-08 15:29:54 +0000

    routing for exopress on prod

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ require("./routes/authRoutes")(app);
 
 require("./routes/billingRoutes")(app);
 
+//heroku
+if (process.env.NODE_ENV === "production") {
+  // express serves up prod files like main.js or main.css files with base of:
+  app.use(express.static("client/build"));
+
+  // express serves index.html if it doesn't know what else to do
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //
 const PORT = process.env.PORT || 5000;
 // console.log(PORT);
