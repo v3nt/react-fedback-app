61aaa2c822e5021d55b3b1f21717a61b329b4eec
v3nt <dan@jynk.net>
2020-12-09 11:56:36 +0000

    Update surveyRoutes.js

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,23 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
-const requireCredits = require'(../middlewares/requireCredits')
+const requireCredits = require("../middlewares/requireCredits");
+
+const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {});
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title, // title:title
+      subject,
+      body,
+      recipients: recipients.split(",").map((email) => {
+        email.trim();
+      }), // es6 refactored
+      _user: req.user.id, // id is from mongoose model.
+      dateSent: Date.now(),
+    });
+  });
+
+  //   full js line recipients: recipients.split(",").map((email) => {return { email: email };
 };
