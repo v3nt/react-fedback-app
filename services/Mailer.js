2b49a2462dfe5093fd088f584d1be2708e808f5a
v3nt <dan@jynk.net>
2020-12-10 07:45:09 +0000

    Update Mailer.js

--- services/Mailer.js
+++ services/Mailer.js
@@ -4,9 +4,44 @@
 const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("../config/keys");
+const recipientSchema = require("../models/Recipient");
 
 class Mailer extends helper.Mail {
-  // this.from_email = new helper.Email('dan@jynk.net');
+  // cons always called first by default
+  constructor({ subject, recipients }, content) {
+    super(); // es2015 classes
+    this.from_email = new helper.Email("dan@jynk.net");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body); // addContent from Mailer (base) class
+    this.addClickTracking(); // ours
+    this.addRecipients(recipients); // ours
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach((recipient) => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+  render() {
+    return "";
+  }
 }
 
 module.exports = Mailer;
