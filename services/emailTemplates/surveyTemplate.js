77f2c3f1cee20828fe8287cd544d6c0be417657a
v3nt <dan@jynk.net>
2020-12-10 09:15:57 +0000

    template updates with backticks

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -2,5 +2,27 @@
 
 module.exports = (survey) => {
   // we will add es6 formats laters
-  return "<p>" + survey + ". HTML email body</p>";
+  // return "<p>" + survey + ". HTML email body</p>";
+  return `
+      <html>
+      <body>
+      <div style="text-align:center;">
+      <h3>We'd like you input!</h3>
+      <p>Please answer the following</p>
+      <p>
+      ${survey.body}
+      </p>
+      <div > 
+       <a href="http://localhost:3000">Yes</a>
+      </div>
+      
+      <div > 
+        <a href="http://localhost:3000">No</a>
+      </div>
+      
+      
+      </div>
+      </body>
+      </html>
+  `;
 };
