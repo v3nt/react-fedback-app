a9fe00efe3c69509859f3af7090e8f5931a0b00b
v3nt <dan@jynk.net>
2020-12-10 06:57:33 +0000

    mailer class started

---
+++ services/Mailer.js
@@ -0,0 +1,15 @@
+// es6 / 2015
+// Files with caps `export` something. ie passport.js doesn't.
+
+const sendgrid = require("sendgrid");
+const helper = sendgrid.mail;
+const keys = require("../config/keys");
+
+class Mailer extends helper.Mail {
+
+
+    this.from_email = new helper.Email('dan@jynk.net');
+
+}
+
+export default Mailer;
