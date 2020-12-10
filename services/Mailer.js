90b7ec166e2f0179be89ada74af2ef5dd8d07300
v3nt <dan@jynk.net>
2020-12-10 07:51:42 +0000

    Update Mailer.js

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,6 +10,7 @@ class Mailer extends helper.Mail {
   // cons always called first by default
   constructor({ subject, recipients }, content) {
     super(); // es2015 classes
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("dan@jynk.net");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -39,9 +40,19 @@ class Mailer extends helper.Mail {
     });
     this.addPersonalization(personalize);
   }
-  render() {
-    return "";
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON(),
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
   }
+  //   render() {
+  //     return "";
+  //   }
 }
 
 module.exports = Mailer;
