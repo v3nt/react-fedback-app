26bc0ace1cc7319028612a719d4a24ed8d794751
v3nt <dan@jynk.net>
2020-12-14 08:52:08 +0000

    valoidate issure persists

--- client/src/components/SurveyField.js
+++ client/src/components/SurveyField.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const SurveyField = ({ input, label, meta }) => {
-  console.log(meta);
+const SurveyField = ({ input, label, meta: { touched, error, warning } }) => {
+  console.log(touched, error, warning);
   return (
     <div className="input-fieldOFF">
       <label>{label}</label>
       <input {...input} />
 
-      {meta.touched && meta.error ? meta.error : null}
+      {touched && error ? error : null}
     </div>
   );
 };
--- client/src/components/SurveyForm.js
+++ client/src/components/SurveyForm.js
@@ -13,64 +13,106 @@ const FIELDS = [
   { label: "Recipients", name: "surveyRecipients", value: "dan@jynk.net" },
 ];
 
-class SurveyForm extends Component {
-  renderField() {
-    return FIELDS.map((el) => {
-      return (
-        <Field key={el.name} {...el} component={SurveyField} type="text" />
-      );
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h3 className="header">Form 2</h3>
-        <form
-          onSubmit={this.props.handleSubmit((values) => console.log(values))}
-        >
-          {this.renderField()}
-
-          <div className="file-field input-field">
-            <div className="btn">
-              <span>File</span>
-              <input type="file" />
-            </div>
-            <div className="file-path-wrapper">
-              <input className="file-path validate" type="text" />
-            </div>
+const renderField = () => {
+  return FIELDS.map((el) => {
+    return <Field key={el.name} {...el} component={SurveyField} type="text" />;
+  });
+};
+const SurveyForm = (props) => {
+  const { error, handleSubmit } = props;
+  return (
+    <div>
+      <h3 className="header">Form 2</h3>
+      <form onSubmit={handleSubmit((values) => console.log(values))}>
+        {renderField()}
+        {error && <strong>{error}</strong>}
+        <div className="file-field input-field">
+          <div className="btn">
+            <span>File</span>
+            <input type="file" />
           </div>
+          <div className="file-path-wrapper">
+            <input className="file-path validate" type="text" />
+          </div>
+        </div>
 
-          <Link to="/surveys" className="red btn">
-            Cancel <i className="material-icons right">clear</i>
-          </Link>
+        <Link to="/surveys" className="red btn">
+          Cancel <i className="material-icons right">clear</i>
+        </Link>
 
-          <button type="submit" className="teal btn-flat right white-text">
-            Next
-            <i className="material-icons right">done</i>
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+        <button type="submit" className="teal btn-flat right white-text">
+          Next
+          <i className="material-icons right">done</i>
+        </button>
+      </form>
+    </div>
+  );
+};
 
-function validate(values) {
+const validate = (values) => {
   const errors = {};
-  console.log("validate", values);
-  // if (values.title) {
-  //   errors.title = "you must provide a title";
-  // }
+  console.log("validate!", values);
+  if (!values.surveyTitle) {
+    errors.title = "you must provide a title";
+  }
+  if (!values.surveyRecipients) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
 
   FIELDS.map((name, label, noValueError) => {
+    console.log(values[name]);
     if (!values[name]) {
       errors[name] = noValueError ? noValueError : `Value needed for ${label}`;
     }
   });
   return errors;
-}
+};
 
 // wrap the form
 export default reduxForm({
   validate,
   form: "surveyForm",
 })(SurveyForm);
+
+// class SurveyForm extends Component {
+//   renderField() {
+//     return FIELDS.map((el) => {
+//       return (
+//         <Field key={el.name} {...el} component={SurveyField} type="text" />
+//       );
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h3 className="header">Form 2</h3>
+//         <form
+//           onSubmit={this.props.handleSubmit((values) => console.log(values))}
+//         >
+//           {this.renderField()}
+
+//           <div className="file-field input-field">
+//             <div className="btn">
+//               <span>File</span>
+//               <input type="file" />
+//             </div>
+//             <div className="file-path-wrapper">
+//               <input className="file-path validate" type="text" />
+//             </div>
+//           </div>
+
+//           <Link to="/surveys" className="red btn">
+//             Cancel <i className="material-icons right">clear</i>
+//           </Link>
+
+//           <button type="submit" className="teal btn-flat right white-text">
+//             Next
+//             <i className="material-icons right">done</i>
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
