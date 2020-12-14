bffe8d4e845c33fd727fc9fce48d53b56c3f827e
v3nt <dan@jynk.net>
2020-12-14 09:24:36 +0000

    validateion finall working - what the OBJ brackets!!!

--- client/src/components/SurveyForm.js
+++ client/src/components/SurveyForm.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form"; // reduxForm -ssame as connect and used to connect to redux store.
 import { Link } from "react-router-dom";
+import _ from "lodash";
+
 import validateEmails from "../utils/validateEmails";
 
 import SurveyField from "./SurveyField";
 
 const FIELDS = [
-  { label: "Title", name: "surveyTitle", noValueError: "Title require" },
-  { label: "Subject", name: "surveySubject" },
+  { label: "Title", name: "title", noValueError: "Title require" },
+  { label: "Subject", name: "subject" },
   { label: "Body", name: "surveyBody" },
-  { label: "Recipients", name: "surveyRecipients", value: "dan@jynk.net" },
+  { label: "Recipients", name: "emails", value: "dan@jynk.net" },
 ];
 
 const renderField = () => {
@@ -17,14 +19,15 @@ const renderField = () => {
     return <Field key={el.name} {...el} component={SurveyField} type="text" />;
   });
 };
+
 const SurveyForm = (props) => {
-  const { error, handleSubmit } = props;
+  const { handleSubmit } = props;
   return (
     <div>
       <h3 className="header">Form 2</h3>
       <form onSubmit={handleSubmit((values) => console.log(values))}>
         {renderField()}
-        {error && <strong>{error}</strong>}
+
         <div className="file-field input-field">
           <div className="btn">
             <span>File</span>
@@ -51,21 +54,22 @@ const SurveyForm = (props) => {
 const validate = (values) => {
   const errors = {};
   console.log("validate!", values);
-  if (!values.surveyTitle) {
+  if (!values.title) {
     errors.title = "you must provide a title";
   }
-  if (!values.surveyRecipients) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
+  if (!values.subject) {
+    errors.subject = "you must provide a subject";
   }
 
-  FIELDS.map((name, label, noValueError) => {
-    console.log(values[name]);
+  FIELDS.map(({ name }) => {
+    console.log("MPA?", name);
     if (!values[name]) {
-      errors[name] = noValueError ? noValueError : `Value needed for ${label}`;
+      errors[name] = `Value needed for ${name}`;
     }
   });
+
+  errors.emails = validateEmails(values.emails || "");
+
   return errors;
 };
 
@@ -74,44 +78,3 @@ export default reduxForm({
   validate,
   form: "surveyForm",
 })(SurveyForm);
-
-// class SurveyForm extends Component {
-//   renderField() {
-//     return FIELDS.map((el) => {
-//       return (
-//         <Field key={el.name} {...el} component={SurveyField} type="text" />
-//       );
-//     });
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <h3 className="header">Form 2</h3>
-//         <form
-//           onSubmit={this.props.handleSubmit((values) => console.log(values))}
-//         >
-//           {this.renderField()}
-
-//           <div className="file-field input-field">
-//             <div className="btn">
-//               <span>File</span>
-//               <input type="file" />
-//             </div>
-//             <div className="file-path-wrapper">
-//               <input className="file-path validate" type="text" />
-//             </div>
-//           </div>
-
-//           <Link to="/surveys" className="red btn">
-//             Cancel <i className="material-icons right">clear</i>
-//           </Link>
-
-//           <button type="submit" className="teal btn-flat right white-text">
-//             Next
-//             <i className="material-icons right">done</i>
-//           </button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
