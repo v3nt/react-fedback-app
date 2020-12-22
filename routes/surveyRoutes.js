0cfb367de45a29a4e0ea8818312e3d09d678dfa3
v3nt <dan@jynk.net>
2020-12-22 21:48:34 +0000

    sendmail webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,6 +4,10 @@ const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
+
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
@@ -12,7 +16,23 @@ module.exports = (app) => {
   });
   //
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      console.log(p.test(pathname));
+      const match = p.test(pathname); // can't deconstruct this as may return null.
+      if (match) {
+        return { email: email, surveyId: match.surveyId, choice: match.choice };
+      }
+    });
+    console.log(events);
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+
+    console.log(uniqueEvents);
+
     res.send({});
   });
   //
